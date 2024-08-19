1fc6daf9cb9f443b9395c9288942a7fd4d539c37
AKHILTH0MAS <akhilpthomas466@gmail.com>
2024-08-19 10:21:49 +0530

    pushhhh

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ let readedWord = "";
 let readedLetters = "";
 let readingLetter = "";
 let clickCount = 0;
+let focusCounter = 1;
+
+let capslock = false;
 
 let mousePad = document.querySelector(".mousePad");
 let recepientText = document.getElementById("recepientText");
+let subjectText = document.getElementById("subjectText");
+let bodyText = document.getElementById("bodyText");
+
+let focusText;
+
 let previewWord = document.querySelector(".previewWord");
 let previewMorse = document.querySelector(".previewMorse");
 let previewLetterTyping = document.querySelector(".previewLetterTyping");
@@ -65,7 +73,7 @@ let possibleCombinations = new Map([
 
 function verifyLetter() {
   if (possibleCombinations.has(readingLetter)) {
-    let letter = possibleCombinations.get(readingLetter);
+    let letter = possibleCombinations.get(readingLetter);   
     previewLetterTyping.innerHTML = letter;
     readedLetters = readedLetters.concat(letter);
     previewWord.innerHTML = readedWord;
@@ -74,6 +82,7 @@ function verifyLetter() {
     return "";
   }
 }
+
 function registerclick() {
   if (!preventClick) {
     clickCount++;
@@ -84,13 +93,17 @@ function registerclick() {
         doubleClickEvent();
       } else if (clickCount == 3) {
         tripleClickEvent();
+      } else if (clickCount == 4) {
+        backspace();
       }
       clickCount = 0;
-    }, 600);
+    }, 1000);
   }
 }
+
 let holdtimer;
 let preventClick = false;
+
 mousePad.addEventListener("mousedown", () => {
   preventClick = false;
   holdtimer = setTimeout(() => {
@@ -98,9 +111,11 @@ mousePad.addEventListener("mousedown", () => {
     fourClickEvent();
   }, 1000);
 });
+
 mousePad.addEventListener("mouseup", () => {
   clearTimeout(holdtimer);
 });
+
 mousePad.addEventListener("click", registerclick);
 
 function singleClickEvent() {
@@ -109,12 +124,14 @@ function singleClickEvent() {
   previewMorse.innerHTML = readingLetter;
   verifyLetter();
 }
+
 function doubleClickEvent() {
   console.log("double click");
   readingLetter = readingLetter.concat("_");
   previewMorse.innerHTML = readingLetter;
   verifyLetter();
 }
+
 function tripleClickEvent() {
   console.log("triple click");
   let letter = verifyLetter();
@@ -123,8 +140,8 @@ function tripleClickEvent() {
   previewWord.innerHTML = readedWord;
   console.log(readedWord);
 }
+
 function fourClickEvent() {
-  console.log("fourth click");
   readedWordsArray.push(readedWord);
   readedWord = "";
   readingLetter = "";
@@ -132,9 +149,39 @@ function fourClickEvent() {
   previewLetterTyping.innerHTML = ":)";
   previewWord.innerHTML = ":)";
   console.log(readedWordsArray.toString());
-  let sentence = '';   
-  readedWordsArray.forEach(element => {
-    sentence = sentence.concat(element, "");
-  })
+  let sentence = "";
+  readedWordsArray.forEach((element) => {
+    sentence = sentence.concat(element, " ");
+  });
   recepientText.innerHTML = sentence;
+}
+
+function backspace() {
+  readingLetter = readingLetter.slice(0, readingLetter.length - 1);
+  verifyLetter();
+  previewMorse.innerHTML = readingLetter;
+}
+
+function changeFocus() {
+  if (focusCounter < 4) {
+    focusCounter++;
+  } else {
+    focusCounter = 1;
+  }
+  if (focusCounter == 1) {
+    focusText = recepientText;
+  } else if (focusCounter == 2) {
+    focusText = subjectText;
+  } else if (focusCounter == 3) {
+    focusText = bodyText;
+  }
+}
+
+function CapsLock() {
+  capslock = !capslock;
+}
+
+function EnterKey(){
+  // add next line 
+  // we could use /n
 }
